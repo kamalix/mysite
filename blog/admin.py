b6412302ca75721b2e83fd9cd8cb2c0cd2cb5628
kamalix <147910814+kamalix@users.noreply.github.com>
2023-10-29 08:52:35 +0330

    admin site register & __str__
    +add field in admin.py with make class
     'PostAdmin'
    +use __str__  for make titile & id in admin page for posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = "-empty-"
+    list_display = ('title','counted_views','created_date','published_date','status','updated_date')
+    list_filter = ('status',)
+    ordering = ['-created_date']
+    search_fields = ['title','content']
+
+
+
+admin.site.register(Post,PostAdmin)
+
