1a98ef4243ce204a3666be9e41548cf72a4542d3
kamalix <147910814+kamalix@users.noreply.github.com>
2023-10-31 09:26:25 +0330

    continue...

--- blog/models.py
+++ blog/models.py
@@ -19,4 +19,5 @@ class Post(models.Model):
         ordering = ['-created_date']
 
     def __str__(self):
-        return '{} - {}'.format(self.title,self.id)
+        #return '{} - {}'.format(self.title,self.id)
+        return self.title
