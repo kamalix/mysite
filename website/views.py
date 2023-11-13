122b9bf6a00bff83c1798fc9ece62ac484e05786
kamalix <147910814+kamalix@users.noreply.github.com>
2023-11-13 19:38:54 +0330

    make contact forms

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse,JsonResponse
+from website.models import Contact
+from website.forms import NameForm,ContactForm
 
 
 def index_view(request):
@@ -10,4 +12,23 @@ def about_view(request):
     return render(request,'website/about.html')
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm
+    return render(request,'website/contact.html',{'form':form})
+
+
+
+
+
+def test_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if  form.is_valid():
+            form.save()
+        
+    form = ContactForm()
+
+    return render(request,'test.html',{'form':form})
