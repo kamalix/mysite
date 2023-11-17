58ac690066fcb559f169df5fd91de47150d8eb36
kamalix <147910814+kamalix@users.noreply.github.com>
2023-11-17 20:10:14 +0330

    create comments in model and admin
    create comments in model and admin and showed in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post,Category
+from blog.models import Post,Category,Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -15,6 +15,15 @@ class PostAdmin(SummernoteModelAdmin):
 
 
 
+class CommenstAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = "-empty-"
+    list_display = ('name','post','approved','created_date')
+    list_filter = ('approved','post')
+    search_fields = ['name','post']
+
 admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
+admin.site.register(Comments,CommenstAdmin)
+
 
